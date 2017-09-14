8927c65e3f50a34e751c2daea2d2dcc84458666c
GoodLuckDay <haon4658@gmail.com>
2017-09-14 23:30:36 +0900

    hw01 comment added

--- hw01/hw01_client.c
+++ hw01/hw01_client.c
@@ -21,59 +21,59 @@ struct cal_data
 	char op;
 	int result;
 	short int error;
-};
+};//소켓을 이용을 하여 입력을 받은 계산식을 구조체에 저장을 하여 전송을 하기위해 구조체 선언
 typedef struct cal_data Cal_data;
 
 
 int main(int argc, char **argv){
-	int sockfd, portno, n;
-	int serverlen;
-	struct sockaddr_in serveraddr;
-	struct hostent *server;
-	char *hostname;
-	char buf[BUFSIZE];
-	int left_num,right_num, result;
-	char op[1];
-	Cal_data sdata;
+	int sockfd, portno, n;//소켓을 저장을 하는 변수와 port번호를 저장하는 변수, 전송과 수신의 결과를 저장을 하는 변수 선언
+	int serverlen;//serveraddr의 크기가 저장이되어지는 변수
+	struct sockaddr_in serveraddr;//서버의 주소 정버를 저장을 하는 변수
+	struct hostent *server;//hostent타입의 구조체가 저장이 되어지는 변수
+	char *hostname;//hostname이 저장이 되는 함수
+	char buf[BUFSIZE];//계산식을 입력을 하기위해서 char 배열 선언
+	int left_num,right_num, result;//입력된 계산식의 연산자와 피연산자를 저장을 하기 위한 변수 선언
+	char op;
+	Cal_data sdata;//소켓을 이용을하여 전송을 할 구조체 선언
 	
-	if(argc != 3){
+	if(argc != 3){//ip주소와 port번호를 입력을 하지 않으면 프로그램을 종료
 		fprintf(stderr, "usage: %s <hostname> <port>\n", argv[0]);
 		exit(0);
 	}
-	hostname = argv[1];
-	portno = atoi(argv[2]);
-	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-	if(sockfd < 0 )
+	hostname = argv[1];//hostname을 저장
+	portno = atoi(argv[2]);//port번호를 저장
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);//socket을 생성
+	if(sockfd < 0 )//소켓이 생성이 되지 않았다면 에러 출력
 		error("ERROR opening socket\n");
 	
-	server = gethostbyname(hostname);
-	if(server == NULL){
+	server = gethostbyname(hostname);//호스트 name에 상응하는 hostent타입의 구조체를 얻는다.
+	if(server == NULL){//hostent의 구조체를 얻지 못하였다면 error출력
 		fprintf(stderr,"ERROR, no such host as %s\n", hostname);
 		exit(0);
 	}
 	
-	bzero((char *) &serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);
-	serveraddr.sin_port = htons(portno);
+	bzero((char *) &serveraddr, sizeof(serveraddr));//버퍼 초기화
+	serveraddr.sin_family = AF_INET;//IPv4 인터넷 프로토콜
+	bcopy((char *)server->h_addr, (char *)&serveraddr.sin_addr.s_addr, server->h_length);//상대의 ip주소를 sesrveraddr.sin_addr.s_addr에 저장
+	serveraddr.sin_port = htons(portno);//사용할 port번호 설정
 	while(1){
-	bzero(buf,BUFSIZE);
-	printf("> ");
-	fgets(buf, BUFSIZE, stdin);
-	sscanf(buf, "%d%[^0-9]%d", &left_num, op, &right_num);
-	memset((void *)&sdata, 0x00, sizeof(sdata));
-	sdata.left_num = htonl(left_num);
+	bzero(buf,BUFSIZE);//계산식을 입력을 받을 char배열 초기화
+	printf("> ");//계산식 입력 안내 출력
+	fgets(buf, BUFSIZE, stdin);//계산식 입력
+	sscanf(buf, "%d%[^0-9]%d", &left_num, &op, &right_num);//입력이 되어진 계산식의 연산자와 피연산자를 각각에 해당하는 변수에 할당
+	memset((void *)&sdata, 0x00, sizeof(sdata));//구조체를 초기화:
+	sdata.left_num = htonl(left_num);//구조체의 연산자와 피연산자를 나타내는 변수에 값을 초기화
 	sdata.right_num = htonl(right_num);
-	sdata.op = op[0];
-	serverlen = sizeof(serveraddr);
-	n = sendto(sockfd, (char *)&sdata, sizeof(sdata), 0, &serveraddr, serverlen);
-	if(n < 0)
+	sdata.op = op;
+	serverlen = sizeof(serveraddr);//serveraddr의 크기를 serverlen에 저장
+	n = sendto(sockfd, (char *)&sdata, sizeof(sdata), 0, &serveraddr, serverlen);//서버쪽으로 구조체를 전송
+	if(n < 0)//전송 실패시 에러 출력
 		error("ERROR in sendto\n");
-	n = recvfrom(sockfd, (char*)&sdata, sizeof(sdata), 0, &serveraddr, &serverlen);
-	if(n < 0)
+	n = recvfrom(sockfd, (char*)&sdata, sizeof(sdata), 0, &serveraddr, &serverlen);//서버쪽에서 오는 구조체 수신
+	if(n < 0)//수신 실패시 에러 출력
 		error("ERROR in recvfrom\n");
-	result = htonl(sdata.result);
-	printf("%d %c %d = %d\n",left_num, op[0], right_num, result);
+	result = htonl(sdata.result);//계산의 결과값으로 온값의 byteorder를 변경
+	printf("%d %c %d = %d\n",left_num, op, right_num, result);//계산식의 결과를 출력
 	}
 	return 0;
 }
--- hw01/hw01_server.c
+++ hw01/hw01_server.c
@@ -16,7 +16,7 @@ struct cal_data
 	char op;
 	int result;
 	short int error;
-};
+};//client에서 보내는 구조체와 동일한 형식의 구조체 정의
 
 typedef struct cal_data Cal_data;
 
@@ -26,51 +26,49 @@ void error(char *msg){
 }
 
 int main(int argc, char **argv){
-	Cal_data sdata;
-	int left_num, right_num, result;
-	int sockfd;
-	int portno;
-	int clientlen;
-	struct sockaddr_in serveraddr;
-	struct sockaddr_in clientaddr;
-	struct hostent *hostp;
-	char buf[BUFSIZE];
+	Cal_data sdata;//구조체를 선언
+	int left_num, right_num, result;//구조체의 값들을 임시로 저장을 하는 변수
+	int sockfd;//생성이 되어진 소켓이 저장이 되는 변수
+	int portno;//port번호가 저장이 되어지는 변수
+	int clientlen;//clientaddr의 크기가 저장이 되어지는 변수
+	struct sockaddr_in serveraddr;//서버의 주소정보를 저장하는 변수
+	struct sockaddr_in clientaddr;//client의 주소정보를 저장하는 변수
+	struct hostent *hostp;//hostent 타입의 구조체가 저장이 되어지는 변수
 	char *hostaddrp;
 	int optval;
 	int n;
 
-	if(argc != 2){
+	if(argc != 2){//입력된 인자의 개수를 확인을 하고 인자의 개수가 맞지 않으면 오류 출력
 		fprintf(stderr, "usage: %s <port>\n", argv[0]);
 		exit(1);
 	}
 
-	portno = atoi(argv[1]);
-	sockfd =socket(AF_INET, SOCK_DGRAM, 0);
-	if(sockfd < 0)
+	portno = atoi(argv[1]);//port번호 저장
+	sockfd =socket(AF_INET, SOCK_DGRAM, 0);//소켓을 생성 AF_INET은 IPv4의 인터넷프로토콜 SOCK_DGRAM은 udp통신 SOCK_STREAM은 tcp통신
+	if(sockfd < 0)//소켓 생성 실패시 오류 메세지 출력
 		error("ERROR opening socket");
 
 	optval = 1;
 	setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval, sizeof(int));
 
-	bzero((char *) &serveraddr, sizeof(serveraddr));
-	serveraddr.sin_family = AF_INET;
-	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serveraddr.sin_port = htons((unsigned short)portno);
+	bzero((char *) &serveraddr, sizeof(serveraddr));//serveraddr변수 초기화
+	serveraddr.sin_family = AF_INET;//IPv4인터넷 프로토콜 사용
+	serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);//32bit IPV4주소
+	serveraddr.sin_port = htons((unsigned short)portno);//사용할 port번호 초기화
 
-	if(bind(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)
-		error("ERROR on binding");
-	clientlen = sizeof(clientaddr);
+	if(bind(sockfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0)//소켓을 커널에 등록
+		error("ERROR on binding");//등록 실패시 에러메세지 출력
+	clientlen = sizeof(clientaddr);//clientaddr구조체의 크기 저장
 	printf("Start to run server!\n");
 	while(1) {
-		bzero(buf,BUFSIZE);
-		n = recvfrom(sockfd, (char *)&sdata, sizeof(sdata), 0, (struct sockaddr *) &clientaddr, &clientlen);
-		if(n < 0)
+		bzero(&sdata,sizeof(sdata));//sdata 초기화
+		n = recvfrom(sockfd, (char *)&sdata, sizeof(sdata), 0, (struct sockaddr *) &clientaddr, &clientlen);//client로 부터 오는 구조체를 sdata에 저장을 한다.
+		if(n < 0)//수신 실패시 에러메세지 출력
 			error("ERROR in recvfrom");
-		left_num = htonl(sdata.left_num);
+		left_num = htonl(sdata.left_num);//구조체에 저장이 되어진 정보의 byteorder를 변경 후 변수에 저장
 		right_num = htonl(sdata.right_num);
 		
-		switch(sdata.op)
-
+		switch(sdata.op)//switch문을 사용하여 해당 연산자에 대하여 계산결과를 저장
 		{
 			case '+':
 				result = left_num + right_num;
@@ -90,19 +88,19 @@ int main(int argc, char **argv){
 				result = left_num / right_num;
 				break;
 		}
-		hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
-		if(hostp == NULL)
+		hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);//client의 정보를 가지는 hostent구조체 저장
+		if(hostp == NULL)//구조체 반환 실패시 에러 메세지 출력
 			error("ERROR on inet_ntoa\n");
-		hostaddrp = inet_ntoa(clientaddr.sin_addr);
-		if (hostaddrp == NULL)
+		hostaddrp = inet_ntoa(clientaddr.sin_addr);//client의 네트워크 바이트 순서의 32비트 값을 Dotted-Decimal Notation의 주소값으로 변경
+		if (hostaddrp == NULL)//주소정보 저장 실패시 에러 출력
 			error("ERROR on inet_ntoa\n");
-		printf("Client Info : %s (%d)\n", hostaddrp, portno);	
-		printf("Input : %d %c %d\n", left_num, sdata.op,right_num);
-		printf("Result : %d\n", result);
-		sdata.result = htonl(result);
-		n = sendto(sockfd, (char *)&sdata, sizeof(sdata), 0, (struct sockaddr *) &clientaddr, clientlen);
-		
-		if( n < 0)
+		printf("Client Info : %s (%d)\n", hostaddrp, portno);	//client의 주소와 사용중인 port의 번호를 출력
+		printf("Input : %d %c %d\n", left_num, sdata.op,right_num);//수신이 되어진 구조체로부터 얻은 피연산자, 연산자들을 출력
+		printf("Result : %d\n", result);//연산의 결과를 출력
+		sdata.result = htonl(result);//결과값의 byteorder를 변경을 한 후 구조체에 저장
+		n = sendto(sockfd, (char *)&sdata, sizeof(sdata), 0, (struct sockaddr *) &clientaddr, clientlen);//client에 결과값을 전송
+	
+		if( n < 0)//전송 실패시 에러 메세지 출력
 			error("ERROR in sendto");
 	}
 }
