9480977f5490c0d7c1af23d59a54ff4b5b03e30f
GoodLuckDay <haon4658@gmail.com>
2017-09-22 22:04:55 +0900

    hw02 comment added

--- hw02/client.c
+++ hw02/client.c
@@ -6,59 +6,59 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define BUF_SIZE 1024
+#define BUF_SIZE 1024//버퍼의 최대 크기를 나타내는 상수
 #define RLT_SIZE 4
-#define OPSZ 4
+#define OPSZ 4//피연산자의 바이트 크기를 나타내는 상수
 
 void error_handling(char* message);
 
 int main(int argc, char* argv[]) {
 
-	int sock;
-	struct sockaddr_in serv_addr;
-	char opmsg[BUF_SIZE];
-	int result, opnd_cnt, i;
+	int sock;//생성이 되어진 소켓이 저장이 되어진 변수
+	struct sockaddr_in serv_addr;//서버에 대한 정보가 저장이 되는 구조체
+	char opmsg[BUF_SIZE];//
+	int result, opnd_cnt, i;//계산의 결과, 피연산자의 수, 반복문을 위한 변수의 선언
 
-	if(argc !=3) {
+	if(argc !=3) {//Port번호와 IP를 입력하지 않으면 에러를 출력한다.
 		printf("Usage : %s  <IP> <port> \n", argv[0]);
 		exit(1);
 	}
 
-	sock = socket(PF_INET, SOCK_STREAM, 0);
-	if(sock == -1)
+	sock = socket(PF_INET, SOCK_STREAM, 0);//IPv4의 TCP 소켓을 생성
+	if(sock == -1)//소켓 생성 실패시 오류 메세지 출력
 		error_handling("socker() error");
-	
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);
-	serv_addr.sin_port=htons(atoi(argv[2]));
 
-	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) 
-		error_handling("connect() error");
+	memset(&serv_addr, 0, sizeof(serv_addr));//serv_addr 구조체 초기화
+	serv_addr.sin_family=AF_INET;//IPv4 인터넷 프로토콜 사용
+	serv_addr.sin_addr.s_addr=inet_addr(argv[1]);//서버의 주소 저장
+	serv_addr.sin_port=htons(atoi(argv[2]));//Port번호 저장
 
-	fputs("oper count : ", stdout);
+	if(connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)//Connect함수를 이용하여 서버와 연결한다.
+		error_handling("connect() error");//연결 실패 시 에러메세지 출력
+
+	fputs("oper count : ", stdout);//피연산자의 개수를 입력받는다.
 	scanf("%d", &opnd_cnt);
-	opmsg[0] = (char)opnd_cnt;
+	opmsg[0] = (char)opnd_cnt;//피연산자의 개수를 server에 보낼 Buffer에 저장한다.
 
 
-	for(i=0; i<opnd_cnt; i++) {
+	for(i=0; i<opnd_cnt; i++) {//피연산자를 입력받는다.
 		printf("operand %d : ", i+1);
 		scanf("%d", (int*)&opmsg[i*OPSZ+1]);
 	}
 	fgetc(stdin);
-	fputs("operator : ", stdout);
-	scanf("%c", &opmsg[opnd_cnt*OPSZ+1]);
-	write(sock, opmsg, opnd_cnt*OPSZ+2);
-	read(sock, &result, RLT_SIZE);
+	fputs("operator : ", stdout);//연산자를 입력받는다.
+	scanf("%c", &opmsg[opnd_cnt*OPSZ+1]);//연산자를 입력받은 피연산자 다음에 byte에 저장한다.
+	write(sock, opmsg, opnd_cnt*OPSZ+2);//서버에 소켓을 전송한다.
+	read(sock, &result, RLT_SIZE);//result변수에 서버로부터 온 결과 값을 저장한다.
 
-	printf("result = %d\n\n", result);
+	printf("result = %d\n\n", result);//result에 저장된 결과값을 출력한다.
 
-	close(sock);
+	close(sock);//소켓을 종료한다.
 	return 0;
 }
 
 
-void error_handling(char *message) {
+void error_handling(char *message) {//매개변수로 온 message에 저장이 된 문장을 에러메세지로 출력을 해준다.
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
--- hw02/server.c
+++ hw02/server.c
@@ -6,82 +6,82 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
-#define BUF_SIZE 1024
-#define OPSZ 4
+#define BUF_SIZE 1024//버퍼의 최대 크기를 나타낸다
+#define OPSZ 4//피연산자의 크기를 나타낸다.
 
 void error_handling(char* message);//매개변수로온 문장을 에러 메세지로 출력을 하는 함수이다.
 int calculate(int opnum, int opnd[], char operator);//client에서 온 연산자와 피연산자들을 계산을 하여 반환을 한다.
 
 int main(int argc, char* argv[]) {
 
-	int serv_sock, clnt_sock;
-	struct sockaddr_in serv_addr, clnt_addr;
-	socklen_t clnt_adr_sz;//client
+	int serv_sock, clnt_sock;//생성된 server의 소켓을 저장을 하는 변수와, 받아진 client의 socket을 저장하는 변수
+	struct sockaddr_in serv_addr, clnt_addr;//각각 Server와 Client의 정보를 저장을 한다.
+	socklen_t clnt_adr_sz;//clnt_addr의 크기가 저장이 되는 변수
 	char opinfo[BUF_SIZE];
-	int result, opnd_cnt, i;
-	int recv_cnt, recv_len;
+	int result, opnd_cnt, i;//
+	int recv_cnt, recv_len;//
 
 	if(argc !=2) {//인자의 개수가 맞지 않다면 에러를 출력한다.
 		printf("Usage : %s <port> \n", argv[0]);
 		exit(1);
 	}
 
-	serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-	if(serv_sock == -1)
+	serv_sock = socket(PF_INET, SOCK_STREAM, 0);//IPv4의 TCP소켓 생성
+	if(serv_sock == -1)//소켓 생성 실패시 에러메세지 출력
 		error_handling("socker() error");
-	memset(&serv_addr, 0, sizeof(serv_addr));
-	serv_addr.sin_family=AF_INET;
-	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-	serv_addr.sin_port=htons(atoi(argv[1]));
+	memset(&serv_addr, 0, sizeof(serv_addr));//serv_addr 구조체 초기화
+	serv_addr.sin_family=AF_INET;//IPv4 인터넷 프로토콜 사용
+	serv_addr.sin_addr.s_addr=htonl(INADDR_ANY);//32bit IPV4주소
+	serv_addr.sin_port=htons(atoi(argv[1]));//사용할 port번호 초기화
 
-	if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) 
-		error_handling("bind() error");
+	if(bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1) //소켓을 커널에 등록
+		error_handling("bind() error");//등록 실패시 오류 메세지 출
 
-	if(listen(serv_sock, 5) == -1) 
-		error_handling("listne() error");
+	if(listen(serv_sock, 5) == -1) //수신 대기열 5개를 생성
+		error_handling("listen() error");//생성 실패시 오류 메세지 출력
 
 
-	clnt_adr_sz = sizeof(clnt_addr);
+	clnt_adr_sz = sizeof(clnt_addr);//clnt_addr 구조체의 크기를 clnt_adr_sz에 저장
 
 	for(i=0; i<5; i++) {
 		opnd_cnt = 0;
-		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_adr_sz);
-		read(clnt_sock, &opnd_cnt, 1);
+		clnt_sock=accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_adr_sz);//클라이언트 접속 요청 대기 및 허락, 클라이언트와 통신을 위해 새 socket생성
+		read(clnt_sock, &opnd_cnt, 1);//소켓에서 1바이트 만큼 읽고 opnd_cnt에 저장을 한다.
 
 		recv_len = 0;
-		while((opnd_cnt*OPSZ+1) >recv_len) {
+		while((opnd_cnt*OPSZ+1) >recv_len) {//피연산자의 개수만큼 byte를 읽으면 반복문을 종료한다.
 			recv_cnt=read(clnt_sock, &opinfo[recv_len], BUF_SIZE-1);
-			recv_len+=recv_cnt;
+			recv_len+=recv_cnt;//읽은 Byte를 recv_len에 더해준다.
 		}
 
-		result = calculate(opnd_cnt, (int*)opinfo, opinfo[recv_len-1]);
-		write(clnt_sock, (char*)&result, sizeof(result));
-		close(clnt_sock);
+		result = calculate(opnd_cnt, (int*)opinfo, opinfo[recv_len-1]);//Calculate함수의 반환값을 result변수에 저장한다.
+		write(clnt_sock, (char*)&result, sizeof(result));//소켓에 result를 저장한다.
+		close(clnt_sock);//client 소켓을 종료한다.
 	}
-	close(serv_sock);
+	close(serv_sock);//server 소켓을 종료한다.
 	return 0;
 
 	
 }
 
 int calculate(int opnum, int opnds[], char op) {
-	int result = opnds[0], i;
-	switch(op) {
+	int result = opnds[0], i;//첫번째 피연산자를 result변수에 저장하고, 반복문을 위해서 변수 i 선언
+	switch(op) {//Switch문을 이용하여 연산자의 종류에 다른 연산을 수행한다.
 		case '+':
-				for(i=1; i<opnum; i++) result +=opnds[i];
+				for(i=1; i<opnum; i++) result +=opnds[i];//두번째 피연산자 부터 마지막 피연산자까지 모두 더해 모든 피연산자를 더한 값을 result변수에 저장
 				break;
 		case '-':
-				for(i=1; i<opnum; i++) result -=opnds[i];
+				for(i=1; i<opnum; i++) result -=opnds[i];//두번째 피연산자 부터 마지막 피연산자까지 모두 빼 모든 피연산자를 뺀 값을 result변수에 저장
 				break;
 		case '*':
-				for(i=1; i<opnum; i++) result *=opnds[i];
+				for(i=1; i<opnum; i++) result *=opnds[i];//두번째 피연산자 부터 마지막 피연산자까지 모두 곱해 모든 피연산자를 곱한 값을 result변수에 저장
 				break;
 	}
-	return result;
+	return result;//계산된 결과값을 반환한다.
 
 }
 
-void error_handling(char *message) {
+void error_handling(char *message) {//에러메세지를 출력을 하는 함수이다.
 	fputs(message, stderr);
 	fputc('\n', stderr);
 	exit(1);
